d55e48aab92fe0bd9236609b7526971c92d571bd
张涛 <zhangtao@zhangtaodeMBP.lan>
2020-05-11 23:18:19 +0800

    添加位运算

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,40 +1,41 @@
 module.exports = {
-    title: 'Tosn',
-    themeConfig: {
-        sidebarDepth: 2,
-        lastUpdated: '更新时间', // string | boolean
-        nav: [
-            {
-                text: 'Vue',
-                link: '/vue/vueCli'
-            },
-            {
-                text: 'Javascript',
-                link: '/javascript/es6'
-            },
-            {
-                text: 'Node',
-                link: '/node/nodeBase'
-            },
-            {
-                text: 'GitHub',
-                link: 'https://github.com/Tosn/Tosn-Blog'
-            }
-        ],
-        sidebar: {
-            '/vue/': [
-                ['vueCli', 'vueCli'],
-                ['vuePress', 'vuePress'],
-                ['vuePlugins', 'vue插件']
-            ],
-            '/javascript/': [
-                ['clever', '巧用方法'],
-                ['es6', 'Es6新增'],
-                ['Array', '数组'],
-                ['Object', '对象'],
-                ['FileSaver', 'FileSaver']
-            ],
-            '/node/': [['nodeBase', 'node基础'], ['koa', 'koa']]
-        }
+  title: 'Tosn',
+  themeConfig: {
+    sidebarDepth: 2,
+    lastUpdated: '更新时间', // string | boolean
+    nav: [
+      {
+          text: 'Vue',
+          link: '/vue/vueCli'
+      },
+      {
+          text: 'Javascript',
+          link: '/javascript/es6'
+      },
+      {
+          text: 'Node',
+          link: '/node/nodeBase'
+      },
+      {
+          text: 'GitHub',
+          link: 'https://github.com/Tosn/Tosn-Blog'
+      }
+    ],
+    sidebar: {
+      '/vue/': [
+        ['vueCli', 'vueCli'],
+        ['vuePress', 'vuePress'],
+        ['vuePlugins', 'vue插件']
+      ],
+      '/javascript/': [
+        ['clever', '巧用方法'],
+        ['es6', 'Es6新增'],
+        ['Array', '数组'],
+        ['Object', '对象'],
+        ['BitOper', '位运算'],
+        ['FileSaver', 'FileSaver']
+      ],
+      '/node/': [['nodeBase', 'node基础'], ['koa', 'koa']]
     }
+  }
 }
